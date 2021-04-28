b3b0cf00173327391dffa53a1845097852037afd
Marc <vengeurk@gmail.com>
2021-04-28 21:03:11 +0200

    Simpler project cards.

--- content.js
+++ content.js
@@ -4,26 +4,18 @@ function generate_project_card(info) {
     let elem = document.createElement('div');
     elem.setAttribute('class', 'col s12 m6 l4');
     elem.innerHTML =
-        '<div class="card sticky-action hoverable">\n' +
-        '    <div class="card-image waves-effect waves-block waves-light">\n' +
-        '        <img class="activator" src="content/images/' + info.image + '" alt="">\n' +
-        '    </div>\n' +
-        '    <div class="card-content">\n' +
-        '        <span class="card-title activator grey-text text-darken-4">'
-        + info.name + '<i class="material-icons right">more_vert</i>' +
-        '        </span>\n' +
-        '    </div>\n' +
-        '    <div class="card-reveal">\n' +
-        '        <span class="card-title grey-text text-darken-4">'
-        + info.name + '<i class="material-icons right">close</i>' +
-        '        </span>\n' +
-        '        <p>' + info.details + '</p>\n' +
-        '    </div>\n' +
-        '\n' +
-        '    <div class="card-action">\n' +
-        '        <a class="' + COLOR_SCHEME + '-text" href="?content=project&project=' + info.id + '">Project Page</a>\n' +
-        '    </div>\n' +
-        '</div>';
+        '<a href="?content=project&project=' + info.id + '">' +
+        '    <div class="card sticky-action hoverable">\n' +
+        '        <div class="card-image waves-effect waves-block waves-light">\n' +
+        '            <img class="activator" src="content/images/' + info.image + '" alt="">\n' +
+        '        </div>\n' +
+        '        <div class="card-content">\n' +
+        '            <span class="card-title activator grey-text text-darken-4">' +
+        '            ' + info.name +
+        '            </span>\n' +
+        '        </div>\n' +
+        '    </div>' +
+        '</a>';
     return elem;
 }
 
